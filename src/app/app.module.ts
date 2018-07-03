bb7eca1dc63f66c57a641b8dd9a4a4576b23c4e5
Jorge Enrique Callisaya Sanchez <sanchezenrique580@gmail.com>
2018-07-03 15:07:52 -0400

    Commit inicial. Creacion de un menu de navegacion, un pie pagina improvisada, y dos contenidos:Inicio y Pokemones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// Enrutamiento
+import { RouterModule, Routes } from '@angular/router';
+// F_Enrutamiento
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { PokemonesComponent } from './components/pokemones/pokemones.component';
+
+const routes: Routes = [
+  { path: 'pokemones', component: PokemonesComponent },
+  { path: 'inicio', component: InicioComponent },
+  { path: '', component: InicioComponent, pathMatch: 'full' },
+  { path: '', component: InicioComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    FooterComponent,
+    InicioComponent,
+    PokemonesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
